03bf9a9eaf113e7adf573a2ca8965d5327ea45fe
vshelnutt <v.shelnutt@aol.com>
2018-07-22 14:13:38 -0500

    connected register route to backend

--- src/App.js
+++ src/App.js
@@ -34,8 +34,26 @@ class App extends Component {
       imageUrl: '',
       box: {},
       route: 'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+
+      }
     }
+  }
+
+  loadUser = (data) => {
+   this.setState({user: {
+      id: data.id,
+    name: data.name,
+    email: data.email,
+    entries: data.entries,
+    joined: data.joined
+   }})
   }
 
 
@@ -102,7 +120,7 @@ onRouteChange = (route) =>  {
     : (
       route === 'signin' 
      ? <Signin onRouteChange={this.onRouteChange} />
-     : <Register onRouteChange={this.onRouteChange} />
+     : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
        )
       
     }
