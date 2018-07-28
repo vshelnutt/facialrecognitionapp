2c6389e393817f8ee229be811b2b92e1b37353cf
vshelnutt <v.shelnutt@aol.com>
2018-07-28 14:49:43 -0500

    refactored all of the routes in the server file

--- facialrecognitionapp-api2/app.js
+++ facialrecognitionapp-api2/app.js
@@ -5,6 +5,9 @@ const cors = require('cors');
 const knex = require('knex');
 
 const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
 
 const app = express();
 app.use(bodyParser.json());
@@ -25,56 +28,13 @@ app.get('/', (req, res) => {
 	res.send(db.users);
 })
 
-app.post('/signin', (req, res) => {
- db.select('email', 'hash').from('login')
- .where('email', '=', req.body.email)
- .then(data => { 
-  const  isValid = bcrypt.compareSync(req.body.password, data[0].hash);
- 	if (isValid) {
-       return  db.select('*').from('users')
-        .where('email', '=', req.body.email)
-        .then(user => {
-          res.json(user[0])
-        })
-        .catch(err => res.status(400).json('unable to get user'))
- 	} else {
- 		res.status(400).json('wrong credentials')
- 	}
- 	
- })
-.catch(err => res.status(400).json('wrong credentials'))  
-})
+app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
 
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
 
+app.get('/profile/:id', (req, res) => {profile.handleProfile(re, res, db)})
 
-app.get('/profile/:id', (req, res) => {
-    const { id } = req.params;
-   db.select('*').from('users').where({
-   	id
-   })
-   .then(user => {
-   	if (user.length) {
-      res.json(user[0])
-   	} else {
-   		res.status(400).json('not found')
-   	}
-  })
-   .catch(err => res.status(400).json('error getting user'))
-})
-
-
-app.put('/image', (req, res) => {
-    const { id } = req.body;
-  db('users')
-.where('id', '=', id)
-.increment('entries', 1)
-.returning('entries')
-.then(entries => {
-	res.json(entries[0]);
-})
-.catch(err => res.status(400).json('unable to get entries'))
-})
+app.put('/image', (req, res) => {image.handleImage(req, res, db)})
 
 app.listen(1337, () => {
 	console.log('app running on port 1337')
