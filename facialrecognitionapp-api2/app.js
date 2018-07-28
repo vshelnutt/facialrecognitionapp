e1b9d015361c5b7fac535fac94af09dc5a930050
vshelnutt <v.shelnutt@aol.com>
2018-07-28 14:08:19 -0500

    refactored the register route

--- facialrecognitionapp-api2/app.js
+++ facialrecognitionapp-api2/app.js
@@ -4,6 +4,8 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 
+const register = require('./controllers/register');
+
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
@@ -18,37 +20,6 @@ const db = knex({
   }
 });
 
-
-
-// const database = {
-// 	users: [
-// 	{
-// 		id: '123',
-// 		name: 'john',
-// 		email: 'john@gmail.com',
-// 		password: 'cookies',
-// 		entries: 0,
-// 		joined: new Date()
-
-// 	},
-// 	{
-// 		id: '124',
-// 		name: 'sally',
-// 		email: 'sally@gmail.com',
-// 		password: 'banannas',
-// 		entries: 0,
-// 		joined: new Date()
-
-// 	}
-// 	],
-// 	login: [
-// 	{
-// 		id: '987',
-// 		hash: '',
-// 		email: 'john@gmail.com'
-// 	}]
-
-// }
 
 app.get('/', (req, res) => {
 	res.send(db.users);
@@ -74,33 +45,8 @@ app.post('/signin', (req, res) => {
 .catch(err => res.status(400).json('wrong credentials'))  
 })
 
-app.post('/register', (req, res) => {
-	const { email, name, password} = req.body;
-	const hash = bcrypt.hashSync(password);
-	db.transaction(trx => {
-		trx.insert({
-			hash: hash,
-			email: email
-		})
-		.into('login')
-		.returning('email')
-		.then(loginEmail => {
-      return trx('users')
-	.returning('*')
-	.insert({
-		email: loginEmail[0],
-		name: name,
-		joined: new Date()
-	})
-  .then(user =>{
-     res.json(user[0]);
-	     })
-	  })
-	 .then(trx.commit)
-	 .catch(trx.rollback)
-	})
-	.catch(err => res.status(400).json('unable to register'))
-})
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
+
 
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
@@ -117,17 +63,6 @@ app.get('/profile/:id', (req, res) => {
    .catch(err => res.status(400).json('error getting user'))
 })
 
-// bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash) {
-//   // Store hash in your password DB.
-// });
-
-// // Load hash from your password DB.
-// bcrypt.compare(myPlaintextPassword, hash, function(err, res) {
-//     // res == true
-// });
-// bcrypt.compare(someOtherPlaintextPassword, hash, function(err, res) {
-//     // res == false
-// });
 
 app.put('/image', (req, res) => {
     const { id } = req.body;
